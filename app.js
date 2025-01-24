0c89842424f14111735cc8f1d0651e371ab7b7c3
fabitmap <fabitmap@gmail.com>
2025-01-24 00:15:22 -0300

    adicionando arquivos

---
+++ app.js
@@ -0,0 +1,44 @@
+const listaAmigos = [];
+const inputAmigo = document.getElementById('amigo');
+const listaAmigosElemento = document.getElementById('listaAmigos');
+const resultado = document.getElementById('resultado');
+
+function adicionarAmigo() {
+    const nomeAmigo = inputAmigo.value.trim();
+
+    if (!nomeAmigo) {
+        alert('Adicione algum nome.');
+        return;
+    }
+
+    listaAmigos.push(nomeAmigo);
+    atualizarLista();
+    inputAmigo.value = '';
+}
+
+inputAmigo.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        adicionarAmigo();
+    }
+});
+
+function atualizarLista() {
+    listaAmigosElemento.innerHTML = '';
+    for (let i = 0; i < listaAmigos.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = listaAmigos[i];
+        listaAmigosElemento.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (!listaAmigos.length) {
+        alert('Adicione amigos antes de sortear');
+        return;
+    }
+
+    const sorteado = Math.floor(Math.random() * listaAmigos.length);
+    const amigo = listaAmigos[sorteado];
+
+    resultado.textContent = amigo;
+}
